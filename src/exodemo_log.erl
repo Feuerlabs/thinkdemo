76a4887962286b1f09527024fc50fe41dd912947
Magnus Feuer <magnus@feuerlabs.com>
2013-01-21 17:02:39 -0800

    Experimented with different ways of getting log data through to server. Server seems to be weird, though

--- src/exodemo_log.erl
+++ src/exodemo_log.erl
@@ -22,6 +22,7 @@
 
 log_can(FrameID, DataLen, Data) ->
     TS = exodemo_lib:make_decimal(exodemo_lib:timestamp()),
+    io:format("log_can(): FrameID ~p Data ~p DataLen ~p ~n", [ FrameID, Data, DataLen ]),
     %% FIXME: ms_timestamp() should be millisec_timestamp() (ms since epoch).
     gen_server:cast(?MODULE, {log_can, TS, FrameID, DataLen, Data}).
 
@@ -38,11 +39,13 @@ init(_) ->
     {ok, #st{}}.
 
 handle_cast({log_can, TS, FrameID, DataLen, Data}, #st{cfg = Cfg} = S) ->
-    case orddict:find(FrameID, Cfg) of
+    case orddict:find(list_to_binary(integer_to_list(FrameID)), Cfg) of
 	{ok, #i{buf = B, max = Max} = I} ->
+	    io:format("exodemo_log(): Will log Frame ~p  Max ~p DataLen ~p Data ~p~n",
+		      [FrameID, Max, DataLen, Data]),
 	    B1 = log_item({TS,DataLen,Data}, B, Max),
 	    I1 = I#i{buf = B1},
-	    Cfg1 = orddict:store(FrameID, I1, Cfg),
+	    Cfg1 = orddict:store(list_to_binary(integer_to_list(FrameID)), I1, Cfg),
 	    {noreply, S#st{cfg = Cfg1}};
 	error ->
 	    {noreply, S}
@@ -67,6 +70,7 @@ handle_info({timeout, _, {send, FrameID}}, #st{cfg = Cfg} = S) ->
 	    Cfg1 = orddict:store(FrameID, I#i{buf = B1, tref = TRef}, Cfg),
 	    {noreply, S#st{cfg = Cfg1}};
 	error ->
+	    io:format("exodemo_log(timeout): Nope ~p ~p~n", [FrameID, Cfg ]),
 	    {noreply, S}
     end;
 handle_info(_Msg, S) ->
@@ -106,10 +110,13 @@ send_buf({_,B}, ID) ->
 	    new_buf();
 	[_|_] = List ->
 	    io:fwrite("List = ~p~n", [List]),
-	    LogData = [{struct, [{ts, TS},
-				 {'can-frame-id', ID},
+	    LogData = [{struct, [{ts, 1234},
+				 {'can-frame-id', list_to_integer(binary_to_list(ID)) },
 				 {'can-value',
-				  exodemo_lib:can_data_value(Len, Data)}]}
+				  Data
+				  %%list_to_binary(integer_to_list(Data))}]}
+				  %% exodemo_lib:can_data_value(Len, binary:encode_unsigned(Data))
+							    }]}
 		       || {_, {TS, Len, Data}} <- List],
 	    io:fwrite("LogData = ~p~n", [LogData]),
 	    exoport:rpc(
@@ -132,6 +139,8 @@ read_config(#st{cfg = Cfg} = S) ->
 			  Int = to_int(
 				  exodemo_lib:find_val(
 				    <<"sample_interval">>, Attrs, 1000)),
+			  io:format("exodemo_log(): Starting timer for Frame ~p Int ~p~n",
+				    [ID, Int]),
 			  TRef = start_timer(Int, ID),
 			  I = #i{max = Sz, int = Int, tref = TRef},
 			  orddict:store(ID, I, Acc)
